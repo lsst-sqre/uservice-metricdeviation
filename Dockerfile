ac720f22801ae6be506672d465196ff82dbd9e2e
adam <athornton@gmail.com>
2016-12-28 17:18:45 -0700

    Initial commit.

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM       centos:7
+MAINTAINER sqre-admin
+LABEL      version="0.0.1" description="LSST DM/SQuaRE metrics microservice" \
+           name="lsstsqre/uservice-metricdeviation"
+
+USER       root
+RUN        yum update -y && \
+           yum install -y epel-release && \
+           yum repolist && \
+           yum install -y git python-pip python-devel && \
+	   pip install --upgrade pip && \
+           pip install requests sqre-uservice-metricdeviation && \
+           useradd -d /home/flasker -m flasker
+
+USER flasker
+WORKDIR /home/flasker
+EXPOSE 5000
+CMD sqre-uservice-metricdeviation
+	   
---
+++ uservice_metricdeviation/server.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+"""Retrieve QA metrics and report deviation"""
+import json
+import requests
+from apikit import APIFlask as apf
+from apikit import BackendError
+from flask import jsonify, request
+
+
+def server(run_standalone=False):
+    """Create the app and then run it."""
+    # Add "/metric_deviation" for mapping behind api.lsst.codes
+    hosturi = "https://squash.lsst.codes"
+    app = apf(name="uservice-metricdeviation",
+              version="0.0.1",
+              repository="https://github.com/sqre-lsst/" +
+              "sqre-uservice-metricdeviation",
+              description="API wrapper for QA Metric Deviation",
+              route=["/", "/metricdeviation"],
+              auth={"type": "bitly-proxy",
+                    "data": {"username": "",
+                             "password": "",
+                             "endpoint": hosturi + "/oauth2/start"}})
+    app.config["SESSION"] = None
+
+    # Linter can't understand decorators.
+    # pylint: disable=unused-variable
+    @app.route("/")
+    def healthcheck():
+        """Default route to keep Ingress controller happy"""
+        return "OK"
+
+    @app.route("/<metric>")
+    @app.route("/<metric>/<threshold>")
+    @app.route("/metricdeviation/<metric>")
+    @app.route("/metricdeviation/<metric>/<threshold>")
+    def get_metricdeviation(metric, threshold=None):
+        """
+        Proxy for squash.lsst.codes.  We expect the incoming request to have
+        Basic Authentication headers, which will be used to get a GitHub
+        Oauth2 token.
+        """
+        inboundauth = None
+        if request.authorization is not None:
+            inboundauth = request.authorization
+            currentuser = app.config["AUTH"]["data"]["username"]
+            currentpw = app.config["AUTH"]["data"]["password"]
+            if currentuser != inboundauth.username or \
+               currentpw != inboundauth.password:
+                _reauth(app, inboundauth.username, inboundauth.password)
+        else:
+            raise BackendError(reason="Unauthorized", status_code=403,
+                               content="No authorization provided.")
+        session = app.config["SESSION"]
+        url = hosturi + "/dashboard/api/measurements/"
+        params = {"job__ci_dataset": "cfht",
+                  "metric": metric,
+                  "page": "last"}
+        resp = session.get(url, params)
+        if resp.status_code == 403:
+            # Try to reauth
+            _reauth(app, inboundauth.username, inboundauth.password)
+            session = app.config["SESSION"]
+            resp = session.get(url, params)
+        if resp.status_code == 200:
+            return _interpret_response(resp.text, threshold)
+        else:
+            raise BackendError(reason=resp.reason,
+                               status_code=resp.status_code,
+                               content=resp.text)
+
+    @app.errorhandler(BackendError)
+    # pylint: disable=unused-variable
+    def handle_invalid_usage(error):
+        """Custom error handler."""
+        response = jsonify(error.to_dict())
+        response.status_code = error.status_code
+        return response
+    if run_standalone:
+        app.run(host='0.0.0.0', threaded=True)
+
+
+def _reauth(app, username, password):
+    """Get a session with authentication data"""
+    session = requests.Session()
+    session.auth = (username, password)
+    app.config["SESSION"] = session
+
+
+def _interpret_response(inbound, threshold):
+    """Decide whether there's a reportable deviation"""
+    tval = float(threshold)
+    try:
+        robj = json.loads(inbound)
+    except json.decoder.JSONDecodeError as exc:
+        raise BackendError(reason="Could not decode JSON result",
+                           status_code=500,
+                           content=str(exc) + ":\n" + inbound)
+    results = robj.results
+    retdict = {"changed": False}
+    if len(results < 2):
+        # No previous data to compare to!
+        return jsonify(retdict)
+    prev = results[-2]
+    curr = results[-1]
+    if prev.value != curr.value:
+        if prev.value:
+            if (100.0 * (curr.value + 0.0) / (prev.value + 0.0)) > tval:
+                retdict["changed"] = True
+                retdict["current"] = curr.value
+                retdict["previous"] = prev.value
+                retdict["changecount"] = 0
+                if curr.changed_packages:
+                    retdict["changed"] = curr.changed_packages
+                    retdict["changecount"] = len(curr.changed_packages)
+    return jsonify(retdict)
+
+
+def standalone():
+    """Entry point for running as its own executable."""
+    server(run_standalone=True)
+
+if __name__ == "__main__":
+    standalone()
